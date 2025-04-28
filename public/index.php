7851134f81ec930abdf9c28dd34ac77e4fa9fac0
alx <world.zay@gmail.com>
2025-04-28 18:12:09 +0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use Sys\Container;
+use Sys\App;
+
+require '../vendor/autoload.php';
+require '../autoload.php';
+require '../vendor/kaa/src/library.php';
+
+$container = new Container('../application/config/container.php');
+$container->get(App::class)->run();
