25132b5c8125bdc0fdc4c87af28f9deb452b2bd0
alx <world.zay@gmail.com>
2025-04-30 15:41:20 +0300

    build: Create ExceptionHandler

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use Sys\App;
 require '../vendor/autoload.php';
 require '../autoload.php';
 require '../vendor/kaa/src/library.php';
+require '../vendor/kaa/src/exceptionHandler.php';
 
 $container = new Container('../application/config/container.php');
 $container->get(App::class)->run();
