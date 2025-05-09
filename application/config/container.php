5495792937e0627fa9f8963f32d7b5a00e7f03d2
alx <world.zay@gmail.com>
2025-05-09 11:57:16 +0300

    Delete custom Container class

--- application/config/container.php
+++ application/config/container.php
@@ -1,13 +1,8 @@
 <?php declare(strict_types=1);
 
-use Controller\Home;
 use Repository\Repo1;
-use Repository\Repo2;
 use Repository\RepoInterface;
-use Sys\App;
 
 return [
     RepoInterface::class => fn() => new Repo1,
-    Home::class => fn() => new Home($this->get(RepoInterface::class)),
-    App::class => fn() => new App($this->get(Home::class)),
 ];
