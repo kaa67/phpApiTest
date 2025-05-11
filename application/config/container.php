41eea93935434184eb4bf7874af2f7aa9c1a18b5
alx <world.zay@gmail.com>
2025-05-11 10:28:54 +0300

    build: Update config/container.php

--- application/config/container.php
+++ application/config/container.php
@@ -1,8 +1,16 @@
 <?php declare(strict_types=1);
 
+use Psr\Http\Message\ServerRequestInterface;
+use HttpSoft\ServerRequest\ServerRequestCreator;
+use HttpSoft\Emitter\EmitterInterface;
+use HttpSoft\Emitter\SapiEmitter;
+
 use Repository\Repo1;
 use Repository\RepoInterface;
 
 return [
+    ServerRequestInterface::class => fn() => (new ServerRequestCreator())->create(),
+    EmitterInterface::class => fn() => new SapiEmitter,
+
     RepoInterface::class => fn() => new Repo1,
 ];
