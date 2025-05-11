42ef86ad8bb44db3497b48731563bb6ce3ff7c3e
alx <world.zay@gmail.com>
2025-05-11 10:32:18 +0300

    build: Update App.php

--- vendor/kaa/src/App.php
+++ vendor/kaa/src/App.php
@@ -4,14 +4,28 @@ declare(strict_types=1);
 
 namespace Sys;
 
-use Controller\Home;
+use Middleware\CORS;
+use Middleware\RouteMiddleware;
+use Sys\DefaultHandler;
+use Sys\Pipeline\Pipeline;
+use HttpSoft\Emitter\EmitterInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class App
 {
-    public function __construct(private Home $handler){}
+    public function __construct(
+        private ServerRequestInterface $request,
+        private Pipeline $pipeline,
+        private EmitterInterface $emitter,
+        private DefaultHandler $defaultHandler
+    ){}
 
     public function run()
     {
-        echo $this->handler->getAnything();
+        $this->pipeline->pipe(CORS::class);
+        $this->pipeline->pipe(RouteMiddleware::class);
+
+        $response = $this->pipeline->process($this->request, $this->defaultHandler);
+        $this->emitter->emit($response);
     }
 }
