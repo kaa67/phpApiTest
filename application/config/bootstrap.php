fa66ec8c5ea077e3a5f5f477e47396311b73e6b5
alx <world.zay@gmail.com>
2025-05-11 10:26:09 +0300

    build: Create config/bootstrap.php and update index.php

---
+++ application/config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('VENDOR', '../vendor/');
+
+require VENDOR . 'autoload.php';
+require '../autoload.php';
+require VENDOR . 'kaa/src/library.php';
+require VENDOR . 'kaa/src/exceptionHandler.php';
--- public/index.php
+++ public/index.php
@@ -2,10 +2,7 @@
 
 use Sys\App;
 
-require '../vendor/autoload.php';
-require '../autoload.php';
-require '../vendor/kaa/src/library.php';
-require '../vendor/kaa/src/exceptionHandler.php';
+require '../application/config/bootstrap.php';
 
 $builder = new DI\ContainerBuilder();
 $builder->addDefinitions('../application/config/container.php');
